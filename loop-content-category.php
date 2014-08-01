9f9deb3f6c437ec9abf81d1376cdd57115d83033
Jonnyauk <jonny@wonderflux.com>
2014-08-01 09:32:15 +0100

    Text domain for translations
    Setup text domain

--- loop-content-category.php
+++ loop-content-category.php
@@ -9,7 +9,7 @@
 
 	<div class="entry-title">
 
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Read %s', 'wonderflux' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h2>
+		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Read %s', 'wfxgirder' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h2>
 
 	</div>
 
