056b1a2efea99c2f28e5a67621bc0b6676f1de69
Jonnyauk <jonny@wonderflux.com>
2014-08-01 09:31:31 +0100

    Text domain for translations
    Setup text domain

--- loop-content.php
+++ loop-content.php
@@ -7,7 +7,7 @@
 
 <div id="post-<?php the_ID(); ?>" <?php wfx_post_class(''); ?>>
 
-	<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Read %s', 'wonderflux' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h2>
+	<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Read %s', 'wfxgirder' ), the_title_attribute( 'echo=0' ) ); ?>"><?php the_title(); ?></a></h2>
 
 	<div class="entry-content">
 
@@ -25,7 +25,7 @@
 
 	<?php
 	// Link pages
-	wp_link_pages( array( 'before' => '<div class="nav-posts">' . __( 'Pages:', 'wonderflux' ), 'after' => '</div>' ) );
+	wp_link_pages( array( 'before' => '<div class="nav-posts">' . __( 'Pages:', 'wfxgirder' ), 'after' => '</div>' ) );
 	?>
 
 </div>
