f8a2efc4fe5dc58e3aa093018b6096445898c3c6
Jonnyauk <jonny@wonderflux.com>
2014-08-21 09:04:47 +0100

    Update package description

--- header-content.php
+++ header-content.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Header display content used as a fallback in all conditions when a header-content-LOCATION.php doesn't exist in this child theme
- * @package Wonderflux Girder child theme
+ * @package Girder child theme for Wonderflux framework
  */
 ?>
 
