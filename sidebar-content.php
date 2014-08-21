145bc3bbad9e8cce92c94fac32035a3d7c74baee
Jonnyauk <jonny@wonderflux.com>
2014-08-21 09:05:13 +0100

    Update package description

--- sidebar-content.php
+++ sidebar-content.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Sidebar display content used as a fallback in all conditions when a sidebar-content-LOCATION.php doesn't exist in this child theme
- * @package Wonderflux Girder child theme
+ * @package Girder child theme for Wonderflux framework
  */
 ?>
 
