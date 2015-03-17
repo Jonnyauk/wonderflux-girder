559050c5dca8f0c83f9ca57ef4ca1b854d8e9366
Jonnyauk <jonny@wonderflux.com>
2015-03-17 08:52:00 +0000

    Update header and footer to responsive layout
    Update header and footer layout code and config.

--- footer-content.php
+++ footer-content.php
@@ -1,66 +1,63 @@
 <?php
 /**
  * Footer display content - a template part inside footer.php
- * used when a footer-content-LOCATION.php doesn't exist in this child theme
+ * Used when file 'footer-content-{location}.php' doesn't exist in this child theme.
+ * - Using additional 'footer-content-{location}.php' examples (see documentation for all):
+ * footer-content-page.php
+ * footer-content-single.php
+ * footer-content-404.php
  * @package Girder child theme for Wonderflux framework
  */
 ?>
 
-<?php //////// Main full-width footer box start //////// ?>
-<div <?php wfx_css('size=full&id=footer-content'); ?>>
+<div class="row" id="footer-content">
 
-	<?php //////// Quarter box start //////// ?>
-	<div <?php wfx_css('size=quarter&class=footer-secondary-left'); ?>>
+	<?php
+	/*
+	Note the use of Flux Layout CSS rules (sizes are relative to parent container)
+	Inside our 'row' we have some divs using the following classes:
+	- box-1-4 (1 quarter width)
+	- box-1-2 (1 half width)
+	- mq-small-min-box-1-1 (At media query 'small' minimum and smaller, go full width)
+	- mq-small-box-1-2 (At media query 'small', go 1 half width)
+	- mq-tiny-box-1-1 (At media query 'tiny', go full width)
 
-		<div class="box-footer">
-			<div class="wfx-cycle slideshow-1">
-				<div class="clearfix">
-					<h3>Welcome!</h3>
-					<p>I am demonstrating the JQuery cycle plugin, which will cycle through divs (see source footer-content.php)</p>
-				</div>
-				<div class="clearfix">
-					<h3>A Demonstration!</h3>
-					<p>This is the WFX Girder demonstration child theme for Wonderflux - rockin!</p>
-				</div>
-				<div class="clearfix">
-					<h3>A test!</h3>
-					<p>You could put any content you wish here, widgets, display functions, plugins (see source footer-content.php)</p>
-				</div>
-			</div>
-		</div>
+	You can mix and match values if the CSS rule exists, please see Flux Layout
+	output in <head> of site for additional CSS rules: wf-css-flux-layout.php
+	 */
+	?>
 
-	</div>
-	<?php //////// Quarter box end //////// ?>
+	<div class="box-1-4 mq-small-min-box-1-1 footer-primary-left">
 
-	<?php //////// Quarter box start //////// ?>
-	<div <?php wfx_css('size=quarter&class=footer-secondary-right'); ?>>
-
-		<div class="box-footer">
+		<div class="inside-std">
+			<h4>Footer primary left</h4>
 			<p>I could contain any content you like - or why not put in a WordPress widget area?</p>
 		</div>
 
 	</div>
-	<?php //////// Quarter box end //////// ?>
 
-	<?php //////// Half box start ////////
-	/*Note: We have 2 'quarters' above, this is followed by a 'half' below - making a full width row of columns
-	  You MUST SET THE PARAMETER "last" to "Y" on the last column in a row when using wfx_css() */
-	?>
-	<div <?php wfx_css('size=half&class=footer-primary&last=Y'); ?>>
+	<div class="box-1-4 mq-small-box-1-2 mq-tiny-box-1-1 footer-primary-mid">
 
-		<div class="box-footer">
-			<h3>Some content</h3>
-			<p>Here is some more demonstration content for you to edit.</p>
+		<div class="inside-std">
+			<h4>Footer primary middle</h4>
+			<p>I could contain any content you like - or why not put in a WordPress widget area?</p>
 		</div>
 
 	</div>
-	<?php //////// Half box end //////// ?>
+
+	<div class="box-1-2 mq-small-box-1-2 mq-tiny-box-1-1 footer-primary-right">
+
+		<div class="inside-std">
+			<h4>Footer primary right</h4>
+			<p>I could contain any content you like - or why not put in a WordPress widget area?</p>
+		</div>
+
+	</div>
 
 </div>
 
-<?php //////// Secondary full-width footer box start //////// ?>
-<div <?php wfx_css('size=full&id=footer-secondary'); ?>>
-		<div class="box-footer">
-			<p><?php wfx_login_logout('');?></p>
-		</div>
+<div class="row" id="footer-secondary">
+	<div class="inside-std">
+		<p><?php wfx_login_logout('');?></p>
+	</div>
 </div>
